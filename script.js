ee9623a952fd3ca9e787ed9478a9d168560888a9
olamide860 <ojomoolamide52@gmail.com>
2024-11-16 14:19:43 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+let myInput = document.getElementById('value0');
+
+myInput.onclick = function(){
+   calc.display.value+="0" 
+}
+let myInput1 = document.getElementById('value1');
+
+myInput1.onclick = function(){
+   calc.display.value+="1" 
+}
+let myInput2 = document.getElementById('value2');
+
+myInput2.onclick = function(){
+   calc.display.value+="2" 
+}
+let myInput3 = document.getElementById('value+');
+
+myInput3.onclick = function(){
+   calc.display.value+="+" 
+}
+let myInput4 = document.getElementById('value3');
+
+myInput4.onclick = function(){
+   calc.display.value+="3" 
+}
+let myInput5 = document.getElementById('value4');
+
+myInput5.onclick = function(){
+   calc.display.value+="4" 
+}
+let myInput6 = document.getElementById('value5');
+
+myInput6.onclick = function(){
+   calc.display.value+="5" 
+}
+let myInput7 = document.getElementById('value-');
+
+myInput7.onclick = function(){
+   calc.display.value+="-" 
+}
+let myInput8 = document.getElementById('value6');
+
+myInput8.onclick = function(){
+   calc.display.value+="6" 
+}
+let myInput9 = document.getElementById('value7');
+
+myInput9.onclick = function(){
+   calc.display.value+="7" 
+}
+let myInput10 = document.getElementById('value8');
+
+myInput10.onclick = function(){
+   calc.display.value+="8" 
+}
+let myInput11 = document.getElementById('valuex');
+
+myInput11.onclick = function(){
+   calc.display.value+="*" 
+}
+let myInput12 = document.getElementById('value9');
+
+myInput12.onclick = function(){
+   calc.display.value+="9" 
+}
+
+let myInput14 = document.getElementById('valuec');
+
+myInput14.onclick = function(){
+   calc.display.value="";
+}
+
+
+let myInput15 = document.getElementById('value=');
+
+myInput15.onclick = function(){
+  calc.display.value =  eval(calc.display.value) 
+}
+let myInput16 = document.getElementById('value/');
+
+myInput16.onclick = function(){
+   calc.display.value+="/" 
+}
